2bd71310f4809073be539a2f787c7391479fb998
sjoonb <sungjun9808@gmail.com>
2021-12-24 02:58:39 +0900

    Solve #1062 related to brute-force (gold IV)
    but it took so much time.. try again
    
    Feedback: Don't be afraid of code getting longer. It's okay if you set
    the varaiable and function name well.

--- covenant/part2/1062.cpp
+++ covenant/part2/1062.cpp
@@ -5,46 +5,63 @@ using namespace std;
 
 // using bitmask?
 
-vector<int> alpha;
+vector<int> words;
+int nextCand[21];
 
-int dfs(int src, int depth) {
+int numMatchWord(int bit) {
+	int ret = 0;
+	for(int i=0; i<words.size(); ++i)
+		if(!(words[i] & ~bit))
+			ret++;
+	return ret;
+}
+
+int dfs(int src, int depth, int bit=0) {
 	if(depth == 0) {
-		
+		return numMatchWord(bit);
 	}
-	return 0;
+	int ret = 0;
+	for(int next=src+1; next<21; ++next) {
+		int cand = nextCand[next];	
+		bit ^= (1 << cand);
+		ret = max(dfs(next, depth-1, bit), ret);
+		bit ^= (1 << cand);
+	}
+	return ret;
+}
+
+int binarize(const string &word, const string &exception) {
+	int bit = 0;
+	for(int i=0; i<word.size(); ++i) {
+		if(exception.find(word[i]) != string::npos)
+			continue;
+		bit |= (1 << (word[i] - 'a')); 
+	}
+	return bit;	
 }
 
 int solve(int n, int k) {
-	if(k<5)
-		return 0;
-
-	for(int i='a'; i<'z'; ++i) {
-		if(i != 'a' || i != 'n' || i != 't' || i != 'i' || i != 'c')
-			alpha.push_back(i-'a');
+	if(k < 5) {
+		string tmp;
+		for(int i=0; i<n; ++i)
+			cin >> tmp;
+		return 0;	
 	}
-	
-	cout << "\n";
-	for(int i=0; i<21; ++i)
-		cout << (char)(alpha[i] + 'a');
 
-	vector<int> words(n);
-
+	words = vector<int>(n);
+	string exception = "antic";
 	for(int i=0; i<n; ++i) {
-		string str;
-		cin >> str;
-		int word = 0;
-		for(int j=0; j<str.size(); ++j) {
-			word |= (1 << (str[j] - 'a'));
-		}
-		words[i] = word;
+		string word;
+		cin >> word;
+		words[i] = binarize(word, exception);
 	}
-	for(int i=0; i<words.size(); ++i)
-		cout << words[i] << endl;
-
-	int r = k-5;
-
-	int ret = dfs(-1, r);
-
+	int j=0;
+	for(int i=0; i<26; ++i) {
+		if(exception.find(i + 'a') != string::npos)
+			continue;
+		nextCand[j++] = i;
+	}
+	int ret = dfs(-1, k-5);
 	return ret;
 }
 
