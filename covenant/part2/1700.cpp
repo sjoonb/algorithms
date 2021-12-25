b3364b64ef385c9708484e522eed9f6caa189b27
sjoonb <sungjun9808@gmail.com>
2021-12-25 18:12:53 +0900

    Try to solve #1700 related to greedy (gold II)

---
+++ covenant/part2/1700.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+// greedy
+// if new electric is not pulgged in, have to determine what eletric should be plugged off
+// how? find all next electrics sequence untill n-1 electrics be found 
+// then remove not found electric
+// if there are more than two electrics, plugged off random will be okay (may be)
+
+int main() {
+	
+}
