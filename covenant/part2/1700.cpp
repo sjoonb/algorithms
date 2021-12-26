c495e963f9fbbb7809447079e635035a37a2fd19
sjoonb <sungjun9808@gmail.com>
2021-12-27 04:56:50 +0900

    Solve #1700 related to greedy (gold I)
    - took a lot of time.. try again later
    - feedback: is good to use method proof by induction when solving greedy prob

--- covenant/part2/1700.cpp
+++ covenant/part2/1700.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int n, k;
+vector<int> electronics(101);
+vector<bool> isPlugged(101, false);
 
-// greedy
-// if new electric is not pulgged in, have to determine what eletric should be plugged off
-// how? find all next electrics sequence untill n-1 electrics be found 
-// then remove not found electric
-// if there are more than two electrics, plugged off random will be okay (may be)
+void unplug(int idx) {
+	int nCand = n;
+	vector<bool> candPlugged(101, false);
+	for(int i=1; i<101; ++i)
+		candPlugged[i] = isPlugged[i];
+	while(nCand > 1 && idx < k) {
+		int cand = electronics[idx];
+		if(candPlugged[cand]) {
+			candPlugged[cand] = false;
+			nCand--;
+		}
+		idx++;		
+	}
+	for(int i=1; i<101; ++i)
+		if(candPlugged[i]) {
+			isPlugged[i] = false;
+			return;
+		}
+	return;
+}
+
+int solve() {
+	if(n >= k) 
+		return 0;
+	int idx=0;
+	int plugged = 0;
+	while(plugged < n && idx < k) {
+		int e = electronics[idx];
+		if(!isPlugged[e]) {
+			isPlugged[e] = true;
+			plugged++;
+		}
+		idx++;
+	}
+	int plugoff = 0;
+	for(; idx<k; ++idx) {
+		int e = electronics[idx];	
+		if(!isPlugged[e]) {
+			unplug(idx+1);
+			isPlugged[e] = true;
+			plugoff++;
+		}
+	}
+	return plugoff;	
+}
 
 int main() {
-	
+	cin >> n >> k;
+
+	for(int i=0; i<k; ++i)
+		cin >> electronics[i];	
+
+	cout << solve();
 }
