0f71ef6bef803e058a464cf0db53e68b7064463d
sjoonb <sungjun9808@gmail.com>
2021-12-31 12:29:45 +0900

    Solve #1916 with dijkstra & pq (gold V)

--- covenant/part2/1916.cpp
+++ covenant/part2/1916.cpp
@@ -1,46 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 const int INF = 1e9 + 1;
 
 int n, m;
 
-vector<vector<pair<int, int>>> graph;
+vector<vector<pair<int, int> > > graph;
 vector<int> dist;
 vector<bool> isVisited;
-
-int minUnvisitedVertex() {
-	int minVertex = 0;
-	int minDist = INF;
-	for(int i=1; i<=n; ++i)
-		if(!isVisited[i] && dist[i] < minDist) {
-			minDist = dist[i];
-			minVertex = i;	
-		}
-	return minVertex;
-}
+priority_queue<pair<int, int> > pq;
 
 int dijkstra(int src, int dest) {
 	dist = vector<int>(n+1, INF);
 	isVisited = vector<bool>(n+1, false);
 	dist[src] = 0;	
-	for(int i=1; i<=n; ++i) {
-		int here = minUnvisitedVertex();	
-		isVisited[here] = true;
-
+    pq.push(make_pair(0, src));
+    while(!pq.empty()) {
+        int here = pq.top().second;
+        int currDist = -pq.top().first;
+        pq.pop();
+        if(dist[here] < currDist)
+            continue;
 		for(int j=0; j<graph[here].size(); ++j) {
 			int there = graph[here][j].first;
 			int candDist = graph[here][j].second;
-			if(dist[here] + candDist < dist[there])
+			if(dist[here] + candDist < dist[there]) {
 				dist[there] = dist[here] + candDist;
+                pq.push(make_pair(-dist[there], there));
+            }
 		}
-	}
+    }
 	return dist[dest];
 }
 
 void initGraph() {
-	graph = vector<vector<pair<int, int>>>(n+1);	
+	graph = vector<vector<pair<int, int> > >(n+1);	
 	for(int i=0; i<m; ++i) {
 		int u, v, w;
 		cin >> u >> v >> w;
@@ -49,6 +45,9 @@ void initGraph() {
 }
 
 int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n >> m;
 	initGraph();	
 	int src, dest;
