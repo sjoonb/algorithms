0064946ba1350df49fc17398942f289b34707f9a
sjoonb <sungjun9808@gmail.com>
2022-01-18 14:42:49 +0900

    Solve 16930 달리기 (platinum III) *
    bfs, direction, negative, timeout
    
    - Don't use tuple for bfs. Use visit array instead. Because it makes it possible to consider more complex situation.
    - In this case, crossing case for bfs made complexity. To avoid timeout, have to break out from loop when current visitCnt is same or less than next visitCnt. But, if you use tuple for saving dpeth, can't consider the crossing situation.
    
    (example)
    
           2
    1 1 -> 2 3 3 3   <<- it can be 2. But saving depth in tuple can't consider this situation.
           ^
           |
           1

--- covenant/part4/16930.cpp
+++ covenant/part4/16930.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
 using namespace std;
 
 int N, M, K;
-bool isVisited[1000][1000];
+int visitCnt[1000][1000];
 char gym[1000][1000];
 
 const int dy[4] = {-1, 0, 1, 0};
@@ -17,53 +16,30 @@ bool inRange(int y, int x) {
 	return false;
 }
 
-bool canPassThrough(int y, int x, int ny, int nx) {
-	int dy = ny - y;	
-	int dx = nx - x;
-	if(!dx) {
-		if(dy > 0) {
-			for(int i=1; i<=dy; ++i)
-				if(gym[y+i][x] == '#')
-					return false;
-		} else {
-			for(int i=-1; i>=dy; --i)
-				if(gym[y+i][x] == '#')
-					return false;
-		}
-	} else {
-		if(dx > 0) {
-			for(int i=1; i<=dx; ++i)
-				if(gym[y][x+i] == '#')
-					return false;
-		} else {
-			for(int i=-1; i>=dx; --i)
-				if(gym[y][x+i] == '#')
-					return false;
-		}
-	}
-	return true;
-}
-
 int solve(int y, int x, int destY, int destX) {
-	queue<tuple<int, int, int> > q; // y, x, depth;
-	q.push(make_tuple(y, x, 0));
-	isVisited[y][x] = true;
+	queue<pair<int, int> > q; // y, x;
+	q.push(make_pair(y, x));
+	visitCnt[y][x] = 1;
 	while(!q.empty()) {
-		int cy = get<0>(q.front());
-		int cx = get<1>(q.front());
-		int depth = get<2>(q.front());
+		int cy = q.front().first;
+		int cx = q.front().second;
 		q.pop();
 		if(cy == destY && cx == destX) {
-			return depth;
+			return visitCnt[cy][cx] - 1;
 		}
 		for(int k=0; k<4; ++k) {
 			for(int l=1; l<=K; ++l) {
 				int ny = cy + dy[k] * l;
 				int nx = cx + dx[k] * l;
-				if(inRange(ny, nx) && !isVisited[ny][nx] && canPassThrough(cy, cx, ny, nx)) {
-					q.push(make_tuple(ny, nx, depth+1));	
-					isVisited[ny][nx] = true;
-				}
+				if(!inRange(ny, nx) || gym[ny][nx] == '#')
+					break;
+
+				if(visitCnt[ny][nx] == 0) {
+					q.push(make_pair(ny, nx));	
+					visitCnt[ny][nx] = visitCnt[cy][cx] + 1;
+				} else if(visitCnt[ny][nx] <= visitCnt[cy][cx]) {
+					break;
+				}	
 			}
 		}
 	}
@@ -71,6 +47,8 @@ int solve(int y, int x, int destY, int destX) {
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	cin >> N >> M >> K;	
 	for(int i=0; i<N; ++i)
 		for(int j=0; j<M; ++j)
