cbf680cbb0629fc6c3796fbb700df77212f2f19c
sjoonb <sungjun9808@gmail.com>
2022-01-22 13:28:56 +0900

    Solve #5557 1 학년 (gold V) *
    dp, 3d
    
    - Have to consider initial state carefully
    - No need to apply sliding window cause the dp array is small
      enough.

--- covenant/part4/5557.cpp
+++ covenant/part4/5557.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int N;
-int numbers[101];
-long long dp[101][21];
+int numbers[100];
+long long dp[100][21];
 
 long long solve() {
-	dp[0][0] = 1;
-	for(int i=1; i<N; ++i) {
+	dp[0][numbers[0]] = 1;
+	for(int i=1; i<N-1; ++i) {
 		int num = numbers[i];	
 		for(int j=0; j<=20; ++j) {
 			if(j - num >= 0)
@@ -16,12 +16,12 @@ long long solve() {
 				dp[i][j+num] += dp[i-1][j];
 		}
 	}
-	return dp[N-1][numbers[N]];
+	return dp[N-2][numbers[N-1]];
 }
 
 int main() {
 	cin >> N;
-	for(int i=1; i<=N; ++i)
+	for(int i=0; i<N; ++i)
 		cin >> numbers[i];
 	cout << solve();
 }
