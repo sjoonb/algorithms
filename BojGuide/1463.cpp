2ad556cd38861b9224d46b3a3a359185e3c69b82
sjoonb <sungjun9808@gmail.com>
2022-01-24 19:49:53 +0900

    Resolved: #1

---
+++ BojGuide/1463.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int N;
+int dp[1000001];
+
+int dfs(int num) {
+	if(num == 1)
+		return 0;
+	int &ret = dp[num];
+	if(ret != -1)
+		return ret;
+	ret = dfs(num-1) + 1;
+	if(num%3 == 0)
+		ret = min(ret, dfs(num/3) + 1);	
+	if(num%2 == 0)
+		ret = min(ret, dfs(num/2) + 1);	
+	return ret;
+}
+
+int main() {
+	cin >> N;
+	memset(dp, -1, sizeof(dp));
+	cout << dfs(N);	
+}
