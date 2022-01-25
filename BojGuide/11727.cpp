b8e9217fa0eb3c787fccf74ef4a81c72c7d5d03d
sjoonb <sungjun9808@gmail.com>
2022-01-25 11:41:28 +0900

    (#17) Solve
    * resolve: #17

--- BojGuide/11727.cpp
+++ BojGuide/11727.cpp
@@ -9,12 +9,13 @@ int dfs(int i) {
 	if(i >= N)
 		return (i == N) ? 1 : 0;
 	int &ret = dp[i];
-	if(ret != -1)
+	if(ret != -1) {
 		return ret;
+	}
 	ret = 0;
 	ret += dfs(i+1);
 	ret += 2*dfs(i+2);
-	return ret % 10007;
+	return ret %= 10007;
 }
 
 int main() {
