8064a083cf34a99bd5eb7aadcd4c73d0cd8ce905
sjoonb <sungjun9808@gmail.com>
2022-01-29 22:10:54 +0900

    (#55) Solve
    problem was my fault for not thinking about the appropriate if statement

--- BojGuide/2011.cpp
+++ BojGuide/2011.cpp
@@ -11,15 +11,23 @@ int dp[5000];
 int dfs(int i) {
 	if(i == N)
 		return 1;
+	if(pw[i] == 0)
+		return 0;
+
 	int &ret = dp[i];
 	if(ret != -1)
 		return ret;	
-	ret = 0;
-	if(pw[i] != 0)
-		ret += dfs(i+1);
-	if(i+2 <= N && pw[i] != 0 && pw[i] <= 2 && pw[i+1] <=6)
-		ret = (ret + dfs(i+2)) % MOD;
-	return ret;
+
+	ret = dfs(i+1);
+	
+//	if(i+2 <= N && pw[i] >= 1 && pw[i] <= 2 && pw[i+1] >= 0 && pw[i+1] <=6)  // in this case, can't consider 17, 18, 19
+	if(i+2 <= N) {
+		int sum = pw[i] * 10 + pw[i+1];
+		if(sum >= 10 && sum <= 26)
+			ret = (ret + dfs(i+2)) % MOD;
+	}
+
+	return ret %= MOD;
 }
 
 int main() {
@@ -30,4 +38,5 @@ int main() {
 	for(int i=0; i<N; ++i)
 		pw[i] = str[i] - '0';
 	cout << dfs(0);
+	return 0;
 }
