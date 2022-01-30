b22815a787969ac98f2a1549698126b14f05e150
sjoonb <sungjun9808@gmail.com>
2022-01-30 13:27:48 +0900

    (#57) Solve
    and I think it's better solution.

--- BojGuide/11052.cpp
+++ BojGuide/11052.cpp
@@ -5,19 +5,14 @@ using namespace std;
 
 int N;
 int P[10001];
-int dp[10001][1000];
+int dp[10001];
 
-int dfs(int src, int num) {
-	if(num == N)
-		return 0;	
-	int &ret = dp[src][num];
-	if(ret != -1)
-		return ret;
-	ret = 0;
-	for(int i=src; i<=N; ++i)
-		if(num + i <= N)
-			ret = max(ret, dfs(i, num+i) + P[i]); 
-	return ret;		
+void solve() {
+	for(int i=1; i<=N; ++i) {
+		dp[i] = P[i];
+		for(int j=1; j<=i/2; ++j)
+			dp[i] = max(dp[i], dp[i-j] + dp[j]);
+	}
 }
 	
 int main() {
@@ -25,9 +20,7 @@ int main() {
 	cin >> N;
 	for(int i=1; i<=N; ++i)
 		cin >> P[i];	
-	int ret = 0;
-	for(int i=1; i<=N; ++i)
-		ret = max(ret, dfs(i, 0));
-	cout << ret;
+	solve();
+	cout << dp[N];
 	return 0;
 }
