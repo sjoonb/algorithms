ad2d22b8c3f7e7740f731e5aa36cc01364a21013
sjoonb <sungjun9808@gmail.com>
2022-01-31 14:41:49 +0900

    (#73) Timeout

---
+++ BojGuide/11004.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int SIZE = 5e6;
+
+int N, K;
+int seq[SIZE];
+
+int main() {
+	cin >> N >> K;	
+	for(int i=0; i<N; ++i)
+		cin >> seq[i];
+	sort(seq, seq+N);
+	cout << seq[K-1];
+	return 0;
+}
