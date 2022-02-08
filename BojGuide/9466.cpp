9b9c39f0d82f04d7eb9957f6f4cb57f3bc6213e4
sjoonb <sungjun9808@gmail.com>
2022-02-08 16:48:42 +0900

    solve #121
    but need to clarification

---
+++ BojGuide/9466.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int N;
+bool isVisited[100001];
+bool inCurrentCycle[100001];
+
+vector<int> graph;
+vector<int> notInTeam;
+
+void dfs(int here) {
+	notInTeam.push_back(here);
+	isVisited[here] = true;
+	inCurrentCycle[here] = true;
+	int there = graph[here];
+	if(inCurrentCycle[there]) {
+		while(notInTeam.back() != there)
+			notInTeam.pop_back();
+		notInTeam.pop_back();
+		inCurrentCycle[here] = false;
+	} else if(!isVisited[there]) {
+		dfs(there);
+	}
+	inCurrentCycle[here] = false;
+}
+
+int solve() {
+	memset(isVisited, false, sizeof(isVisited));
+	notInTeam = vector<int>();
+	for(int i=1; i<=N; ++i) {
+		if(!isVisited[i]) {
+			dfs(i);
+		}
+	}
+	return notInTeam.size();
+}
+
+int main() {
+	int cases;
+	cin >> cases;
+	for(int cc=0; cc<cases; ++cc) {
+		cin >> N;
+		graph = vector<int>(N+1);
+		for(int i=1; i<=N; ++i)
+			cin >> graph[i];
+		cout << solve() << "\n";
+	}
+	return 0;
+}
+
+
