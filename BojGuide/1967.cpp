80ed33760e3231cf9f3708fc0bd17e50ae152d9b
sjoonb <sungjun9808@gmail.com>
2022-02-10 15:04:50 +0900

    solve #130
    close: #130

---
+++ BojGuide/1967.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int V;
+bool isVisited[100001];
+int child[100001];
+vector<vector<pair<int, int> > > graph;
+
+int dfs(int here) {
+	isVisited[here] = true;
+	int ret = 0;
+	for(int i=0; i<graph[here].size(); ++i) {
+		int there = graph[here][i].first;
+		int dist = graph[here][i].second;
+		if(!isVisited[there]) {
+			int cand = dfs(there) + dist;
+			if(ret < cand) {
+				ret = cand;
+				child[here] = there;
+			}
+		}
+	}
+	return ret;
+}
+
+int solve() {
+	int node = 1;
+	dfs(node);
+	while(child[node] != 0) {
+		node = child[node];
+	}
+	memset(isVisited, false,  sizeof(isVisited));
+	int ret = dfs(node);
+	return ret;
+}
+
+int main() {
+	cin >> V;
+	graph = vector<vector<pair<int, int> > >(V+1);
+	for(int i=0; i<V-1; ++i) {
+		int u, v, w;	
+		cin >> u >> v >> w;
+		graph[u].push_back(make_pair(v, w));
+		graph[v].push_back(make_pair(u, w));
+	}
+	cout << solve();
+	return 0;	
+}
