f9e68f6b9272a31b9cec8a9565a660e59d482a90
sjoonb <sungjun9808@gmail.com>
2022-02-13 10:48:15 +0900

    solve #137
    close: #137
    
    using merge

--- BojGuide/11728.cpp
+++ BojGuide/11728.cpp
@@ -4,20 +4,40 @@
 using namespace std;
 
 int N, M;
-vector<int> seq;
+vector<int> A, B;
+
+vector<int> merge() {
+	int pa, pb;
+	pa = pb = 0;
+	vector<int> ret;
+	while(pa < A.size() && pb < B.size()) {
+		if(A[pa] < B[pb])	
+			ret.push_back(A[pa++]);
+		else
+			ret.push_back(B[pb++]);
+	}
+	while(pa < A.size())
+		ret.push_back(A[pa++]);
+	while(pb < B.size())
+		ret.push_back(B[pb++]);
+	return ret;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cin >> N >> M;
-	for(int i=0; i<N+M; ++i) {
-		int num;
-		cin >> num;
-		seq.push_back(num);
+	A = vector<int>(N);
+	B = vector<int>(M);
+	for(int i=0; i<N; ++i) {
+		cin >> A[i];
 	}
-	sort(seq.begin(), seq.end());	
-	for(int i=0; i<seq.size(); ++i)
-		cout << seq[i] << " ";
+	for(int i=0; i<M; ++i) {
+		cin >> B[i];
+	}
+	vector<int> ret = merge();	
+	for(int i=0; i<ret.size(); ++i)
+		cout << ret[i] << " ";
 	return 0;
 }
 
