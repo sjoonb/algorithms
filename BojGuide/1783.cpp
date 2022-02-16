dcb4601dd9de319c45cf627e40aa36ce6e8d252b
sjoonb <sungjun9808@gmail.com>
2022-02-16 13:20:58 +0900

    solve #148
    optimization

--- BojGuide/1783.cpp
+++ BojGuide/1783.cpp
@@ -7,23 +7,10 @@ int solve() {
 	if(N == 1) {
 		return 1;	
 	} else if(N == 2) {
-		if(M < 3)
-			return 1;
-		else if(M < 5)
-			return 2;
-		else if(M < 7)
-			return 3;
-		else 
-			return 4;
-	} else {
-		if(M < 2)
-			return 1;
-		else if(M < 3)
-			return 2;
-		else if(M < 4)
-			return 3;
-		else if(M < 7)
-			return 4;
+		return min(3, (M+1) / 2);
+	} else if(N >= 3) {
+		if(M <= 6)
+			return min(M, 4);
 		else
 			return 5 + (M - 7);
 	}
