81c553817e8c8e0a0a6735bbe98480fc9324b439
sjoonb <sungjun9808@gmail.com>
2022-03-01 13:08:38 +0900

    solve #97

--- BojGuide/1373.cpp
+++ BojGuide/1373.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-string str;
-
-int convert(string bin) {
-	int ret = 0;
-	int square = 0;
-	for(int i=bin.size()-1; i>=0; --i)	
-		ret += (bin[i] - '0') * pow(2, square++);
+int bin_to_oct(int bin) {
+	int ret = 0, square = 0;
+	while(bin > 0) {
+		ret += bin % 10 * pow(2, square++);
+		bin /= 10;
+	}
 	return ret;
 }
 
-void solve() {
-	int r = str.size() % 3;
-
-	if(r != 0)
-		cout << convert(str.substr(0, r));
-
-	for(int i=r; i<str.size(); i+=3)
-		cout << convert(str.substr(i, 3));
-				
+string convert(string binary) {	
+	string ret;
+	for(int i=0; i<binary.size(); i+=3) {
+		int bin = stoi(binary.substr(i, 3));
+		int hex = bin_to_oct(bin);
+		cout << hex;
+	}
+	return ret;
 }
 
 int main() {
-	cin >> str;	
-	solve();	
+	string binary;	
+	cin >> binary;	
+	while(binary.size() % 3 != 0) {
+		binary.insert(0, "0");		
+	}
+	cout << convert(binary);
 	return 0;
 }
