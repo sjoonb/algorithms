e480b6fb9eb732d50bc842ae79233001fdec46a1
sjoonb <sungjun9808@gmail.com>
2022-03-04 20:00:44 +0900

    solve #131

--- BojGuide/1654.cpp
+++ BojGuide/1654.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int N, K;
-vector<int> wires;
+#define ll long long
+
+int K, N;
+int wires[100000];
 
 int f(int x) {
-	int ret = 0;	
-	for(int i=0; i<K; ++i)
-		ret += (wires[i] / x);
+	int ret = 0;
+	for(int i=0; i<K; ++i)	
+		ret += wires[i] / x;
 	return ret;
 }
 
-int bisection(long long lo, long long hi) {
-	for(int i=0; i<100; ++i) {
-		int mid = (lo + hi) / 2;
+int bisection() {
+	ll lo = 0, hi = (1ll << 31); 	
+	for(int iter=0; iter<100; ++iter) {
+		ll mid = (lo + hi) / 2;	
 		int fmid = f(mid);
-		if(fmid < N)
-			hi = mid;
-		else if(fmid >= N)
+		if(fmid >= N)
 			lo = mid;
+		else
+			hi = mid;
 	}
 	return (lo + hi) / 2;
 }
 
 int main() {
-	cin >> K >> N;
-	wires = vector<int>(K);
+	cin >> K >> N;	
 	for(int i=0; i<K; ++i)
 		cin >> wires[i];
-	cout << bisection(1, (unsigned int) 1 << 31);
-	return 0;	
+	cout << bisection();
+	return 0;
 }
