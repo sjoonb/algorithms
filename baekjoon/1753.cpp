28ea45b3f44b6472f0f37b709cd96406efb98d27
sjoonb <sungjun9808@gmail.com>
2022-03-17 11:50:18 +0900

    solve #1753

--- baekjoon/1753.cpp
+++ baekjoon/1753.cpp
@@ -1,51 +1,52 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
 using namespace std;
 
-#define INF 987654321
-
+#define INF (1<<30)
+typedef vector<vector<pair<int, int> > > Graph;
 int V, E, K;
+Graph graph;
 vector<int> dist;
-vector<vector<pair<int, int> > > adjList;
+vector<bool> isVisited;
 
-void dijkstra(int start) {
-	priority_queue<pair<int ,int>> pq;
-	pq.push({0, start});
+void dijkstra(int src) {
+	dist[src] = 0;
+	priority_queue<pair<int, int> > pq;
+	pq.push({0, src});
 	while(!pq.empty()) {
-		int w = -pq.top().first;		
 		int here = pq.top().second;
 		pq.pop();
-		if(dist[here] != INF)
+		if(isVisited[here])
 			continue;
-		dist[here] = w;
-		for(int j=0; j<adjList[here].size(); ++j) {
-			int there = adjList[here][j].first;
-			int cost = adjList[here][j].second;
-			if(dist[there] == INF)
-				pq.push({-(dist[here] + cost), there});
+		isVisited[here] = true;
+		for(int i=0; i<graph[here].size(); ++i) {
+			int there = graph[here][i].first;
+			int cost = graph[here][i].second;
+			if(dist[there] > dist[here] + cost) {
+				dist[there] = dist[here] + cost;
+				pq.push({-dist[there], there});
+			}
 		}
 	}
-
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cin >> V >> E >> K;
+	graph = Graph(V+1);
 	dist = vector<int>(V+1, INF);
-	adjList = vector<vector<pair<int, int> > >(V+1);
+	isVisited = vector<bool>(V+1, false);
 	for(int i=0; i<E; ++i) {
 		int u, v, w;
 		cin >> u >> v >> w;
-		adjList[u].push_back({v, w});
+		graph[u].push_back({v, w});
 	}
 	dijkstra(K);
 	for(int i=1; i<=V; ++i)
-		if(dist[i] == INF)
-			cout << "INF" << "\n";
-		else
+		if(dist[i] != INF)
 			cout << dist[i] << "\n";
+		else
+			cout << "INF\n";
 	return 0;
 }
